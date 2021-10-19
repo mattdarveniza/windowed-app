c9082a1dd6d3b0ab8026811dd46bd0668cbe9519
Matt Darveniza <matt.darveniza@navis.com>
2021-10-19 15:20:32 +1100

    window manager that portals ui state from one window to another

---
+++ src/WindowManager.tsx
@@ -0,0 +1,71 @@
+import { ReactNode, useState } from "react";
+import { createPortal } from "react-dom";
+
+type WindowProps = {
+  w: Window;
+  children: ReactNode;
+};
+
+const Window = ({ w, children }: WindowProps) => {
+  const rootNode = w.document.getElementById("root");
+  if (!rootNode) {
+    throw Error("No root node found");
+  }
+
+  return createPortal(children, rootNode);
+};
+
+export const WindowManager = () => {
+  const [windows, setWindows] = useState<Window[]>([]);
+  const [list, setList] = useState<string[]>([]);
+  const [textField, setText] = useState<string>("");
+
+  const addToList = () => setList((list) => [...list, textField]);
+
+  const createWindow = () => {
+    const newWindow = window.open(
+      "",
+      `popout${windows.length}`,
+      "height=500,width=500,left=500,top=100"
+    );
+    if (newWindow === null) {
+      window.alert("enable popups or something");
+      return;
+    }
+
+    newWindow.document.body.innerHTML = '<div id="root"></div>';
+
+    setWindows([...windows, newWindow]);
+  };
+
+  return (
+    <>
+      {windows.map((w) => (
+        <Window w={w}>
+          <ul>
+            {list.map((item) => (
+              <li>{item}</li>
+            ))}
+          </ul>
+        </Window>
+      ))}
+      <div>
+        <button type="button" onClick={createWindow}>
+          New Window
+        </button>
+      </div>
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          addToList();
+        }}
+      >
+        <input type="text" onChange={(e) => setText(e.target.value)} />
+        <button type="submit" disabled={!textField}>
+          +
+        </button>
+      </form>
+    </>
+  );
+};
